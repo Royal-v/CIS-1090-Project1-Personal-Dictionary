3c73dc65668e73a4faf7501c7dfed34694f34958
PSC Student <77242772+Royal-v@users.noreply.github.com>
2022-10-29 09:22:23 -0400

    Update personal-dictionary.js

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,1 +1,16 @@
 //Fill in this empty file
+let words= [];
+function search(word){
+    let position = 0;
+    while(position <= personaldictionary.length - 1){
+        if(database[position] == word){
+            return true;
+        }
+        else(position++)
+    }
+    return false;
+}
+function addWord(word){
+    words.push(word);
+}
+export{search, addWord};
